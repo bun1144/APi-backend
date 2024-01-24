40de29916184af85be76b99104bb3c37f5d410cf
St@rs;ArcLIght <kasin123sirichan@gmail.com>
2024-01-25 04:57:52 +0700

    complete

--- book.py
+++ book.py
@@ -13,11 +13,7 @@ client = MongoClient(uri)
 db = client["students"]
 collection = db["std_info"]
 
-books=[
-    {"id":1,"title":"Book 1","author":"Author 1"},
-    {"id":2,"title":"Book 2","author":"Author 2"},
-    {"id":3,"title":"Book 3","author":"Author 3"}
-]
+
 stds=[]
 all_students = collection.find()
 for std in all_students:
